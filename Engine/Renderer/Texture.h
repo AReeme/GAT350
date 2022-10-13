b2be39690f51e18e245a4705fb6ef2570d6e5cf8
AReeme <areeme@student.neumont.edu>
2022-10-13 16:24:04 -0600

    Classwork 10/13

--- Engine/Renderer/Texture.h
+++ Engine/Renderer/Texture.h
@@ -1,15 +1,14 @@
 #pragma once
 #include "Math/Vector2.h"
 #include "Resource/Resource.h"
+#include "Renderer.h"
 #include <string>
 
 struct SDL_Texture;
 struct SDL_Surface;
-// !! forward declaration for SDL pointers below (SDL likes to use structs)
 
 namespace neu
 {
-	// !! forward declaration for Renderer below
 	class Renderer;
 
 	class Texture : public Resource
@@ -23,11 +22,16 @@ namespace neu
 
 		bool Load(const std::string& filename, Renderer& renderer);
 
+		void Bind() { glBindTexture(m_target, m_texture); }
+
 		Vector2 GetSize() const;
 
 		friend class Renderer;
 
 	private:
-		SDL_Texture* m_texture = nullptr;
+		GLuint m_texture = 0;
+		GLenum m_target = GL_TEXTURE_2D;
+		GLuint m_unit = GL_TEXTURE0;
+		//SDL_Texture* m_texture = nullptr;
 	};
 }
