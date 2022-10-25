40dc7e5dda2d9f48edd7c3dd39541780c0958b54
AReeme <areeme@student.neumont.edu>
2022-10-25 13:17:12 -0600

    Update Scene to Resource

--- Application/Main.cpp
+++ Application/Main.cpp
@@ -60,54 +60,13 @@ int main(int argc, char** argv)
 	LOG("Window Created...");
 
 	// load scene 
-	auto scene = std::make_unique<neu::Scene>();
-
-	rapidjson::Document document;
-	bool success = neu::json::Load("scenes/basic.scn", document);
-	if (!success)
-	{
-		LOG("error loading scene file %s.", "scenes/basic.scn");
-	}
-	else
-	{
-		scene->Read(document);
-		scene->Initialize();
-	}
-
-	// Create Vertex Buffer
-	/*std::shared_ptr<neu::VertexBuffer> vb = neu::g_resources.Get<neu::VertexBuffer>("box");
-	vb->CreateVertexBuffer(sizeof(vertices), 36, vertices);
-	vb->SetAttribute(0, 3, 8 * sizeof(float), 0);
-	vb->SetAttribute(1, 3, 8 * sizeof(float), 3 * sizeof(float));
-	vb->SetAttribute(2, 3, 8 * sizeof(float), 6 * sizeof(float));*/
-
-	// create material 
-	std::shared_ptr<neu::Material> material = neu::g_resources.Get<neu::Material>("Materials/Box.mtrl");
-	material->Link();
-	material->Bind();
-
-	glm::mat4 model{ 1 };
-	glm::mat4 projection = glm::perspective(45.0f, neu::g_renderer.GetWidth() / (float)neu::g_renderer.GetHeight(), 0.01f, 100.0f);
-
-	glm::vec3 cameraPosition = glm::vec3{ 0, 2, 2 };
-	float speed = 10;
-
-	std::vector<neu::Transform> transforms;
-	for (size_t i = 0; i < 10; i++)
-	{
-		transforms.push_back({ {  neu::randomf(-10, 10), neu::randomf(-10, 10),  neu::randomf(-10, 10)}, {neu::randomf(360), 90,  0} });
-	}
-
-	auto m = neu::g_resources.Get<neu::Model>("models/ogre.obj");
+	auto scene = neu::g_resources.Get<neu::Scene>("scenes/basic.scn");
 
 	bool quit = false;
 	while (!quit)
 	{
 		neu::Engine::Instance().Update();
 		if (neu::g_inputSystem.GetKeyState(neu::key_escape) == neu::InputSystem::KeyState::Pressed) quit = true;
-
-		glm::mat4 view = glm::lookAt(cameraPosition, glm::vec3{ 0, 0, 0 }, glm::vec3{ 0, 1, 0 });
-		//model = glm::eulerAngleXYZ(0.0f, neu::g_time.time, 0.0f);
 
 		scene->Update();
 
